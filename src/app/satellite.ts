6341f2b0eded1db0f973f3d4b09829d2b652798d
Matt Newbold <matt.newbold208@gmail.com>
2020-12-16 14:30:03 -0600

    first assign 6 commit

---
+++ src/app/satellite.ts
@@ -0,0 +1,10 @@
+export class Satellite {
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+
+    }
+    this.name: string;
+    this.orbitTye: string;
+    this.type: string;
+    this.operational: boolean;
+    this.launchDate : string;
+}
