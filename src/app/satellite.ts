f01644e2ca4e348c21cc8f7a7f8eed249f792440
Matt Newbold <matt.newbold208@gmail.com>
2020-12-21 11:07:56 -0600

    orbit report 2nd save

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -1,10 +1,15 @@
 export class Satellite {
+    name: string;
+    orbitTye: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
     constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
-
+        this.name = name
+        this.orbitTye = orbitType
+        this.type = type
+        this.operational = operational
+        this.launchDate =launchDate
+   
     }
-    this.name: string;
-    this.orbitTye: string;
-    this.type: string;
-    this.operational: boolean;
-    this.launchDate : string;
 }
